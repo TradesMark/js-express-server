18d338c5defc96da26c6117694a200a368a48f3b
Anton Galkin <artemis9898@mail.ru>
2018-03-20 15:46:12 +0300

    Add files via upload

--- js-express-server/src/index.js
+++ js-express-server/src/index.js
@@ -9,7 +9,6 @@ app
     .get(/users/, async r => {
         const { data: { users: items } } = await get(URL);
         r.res.render('list', { title: 'Список логинов', items });
-
     })
     .use(r => r.res.status(404).end('Still not here, sorry!'))
     .use((e, r, res, n) => res.status(500).end('Error: ${e}'))
